8da6834fa749601bcd9385f62ba4172b337e0ecc
Kevin St.Onge <kevinstonge@gmail.com>
2020-10-06 20:54:16 -0400

    edit and delete working

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
 
-function Movie({ addToSavedList }) {
+function Movie({ addToSavedList, movieList, setMovieList }) {
+  const { push } = useHistory();
   const [movie, setMovie] = useState(null);
   const params = useParams();
 
@@ -16,6 +17,14 @@ function Movie({ addToSavedList }) {
 
   const saveMovie = () => {
     addToSavedList(movie);
+  };
+  const deleteMovie = () => {
+    axios.delete(`http://localhost:5000/api/movies/${movie.id}`).then((r) => {
+      if (r.status === 202) {
+        setMovieList(movieList.filter((m) => m.id !== movie.id));
+        push("/");
+      }
+    });
   };
 
   useEffect(() => {
@@ -36,7 +45,15 @@ function Movie({ addToSavedList }) {
           Save
         </button>
 
-        <button className="edit-button">Edit</button>
+        <button
+          className="edit-button"
+          onClick={() => push(`/update-movie/${movie.id}`)}
+        >
+          Edit
+        </button>
+        <button className="delete-button" onClick={() => deleteMovie()}>
+          Delete
+        </button>
       </div>
     </div>
   );
--- client/src/Movies/UpdateMovie.js
+++ client/src/Movies/UpdateMovie.js
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import axios from "axios";
 const UpdateMovie = (props) => {
+  const { push } = useHistory();
+  const { id } = useParams();
+  const [movie, setMovie] = useState({
+    title: "",
+    director: "",
+    metascore: "",
+    actors: [],
+  });
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000/api/movies/${id}`)
+      .then((r) => setMovie(r.data));
+  }, [id]);
+  const handleChange = (e) => {
+    setMovie({ ...movie, [e.target.name]: e.target.value });
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .put(`http://localhost:5000/api/movies/${id}`, movie)
+      .then((r) => {
+        props.setMovieList(
+          props.movieList.map((m) => (m.id === movie.id ? movie : m))
+        );
+        push("/");
+      })
+      .catch((e) => console.log(e));
+  };
   return (
-    <form>
-      <input type="text" />
+    <form className="update-movie" onSubmit={onSubmit}>
+      {["title", "director", "metascore"].map((item) => {
+        return (
+          <label htmlFor={item}>
+            {item}:
+            <input
+              type="text"
+              id={item}
+              name={item}
+              value={movie[item]}
+              onChange={handleChange}
+            />
+          </label>
+        );
+      })}
+      <button type="submit">Save</button>
     </form>
   );
 };
